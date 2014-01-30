f729118951212d75668e81403eb3b250cf7b1a9d
Andreas <acbueff@gmail.com>
2014-01-30 12:57:07 -0800

    corrected println statement

--- GitTestJava/src/TestInGit.java
+++ GitTestJava/src/TestInGit.java
@@ -4,7 +4,7 @@ public class TestInGit {
 	public static void main(String[] args){
 		System.out.println("Let there be light!");
 		System.out.println("Git is easy to use dude!");
-		System.out.prrintln("Does each edit of this class require git add?");
+		System.out.println("Does each edit of this class require git add?");
 	}
 
 }
