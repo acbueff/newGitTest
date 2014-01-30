7b311b06baa722464b839068b1d975220fde1d84
Andreas <acbueff@gmail.com>
2014-01-30 13:16:40 -0800

    NOW I added a second java class

---
+++ GitTestJava/src/secondTestInGit.java
@@ -0,0 +1,18 @@
+
+public class secondTestInGit {
+
+	public static void derp(String herp){
+		System.out.println("When you derp you " + herp);
+		
+	}
+	
+	public static void main(String[] args){
+		
+		String herp = "herpdederp";
+		
+		System.out.println("Is playing with strings even real programming?");
+		derp(herp);
+		derp("Chili Ring Fry");
+	}
+	
+}
