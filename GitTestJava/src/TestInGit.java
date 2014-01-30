bf5782ed921061163f33d52bfdf1011197324fbc
Andreas <acbueff@gmail.com>
2014-01-30 12:54:43 -0800

    third string statement added to TestInGit

--- GitTestJava/src/TestInGit.java
+++ GitTestJava/src/TestInGit.java
@@ -4,6 +4,7 @@ public class TestInGit {
 	public static void main(String[] args){
 		System.out.println("Let there be light!");
 		System.out.println("Git is easy to use dude!");
+		System.out.prrintln("Does each edit of this class require git add?");
 	}
 
 }
